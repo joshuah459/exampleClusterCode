be1497bad30732ceba356b368ff84a16c73fafdd
joshuah459 <joshuah459@gmail.com>
2021-04-18 15:15:27 -0500

    Update RankedHelloWorld.py
    modified tutorial code

--- 02 Hello World with Rank/RankedHelloWorld.py
+++ 02 Hello World with Rank/RankedHelloWorld.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
-
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.rank
 
-if rank == 0:
-    data = {'a':1,'b':2,'c':3}
-else:
-    data = None
+data="Hello World"
 
-data = comm.bcast(data, root=0)
-print 'rank',rank,data
+print 'rank: ',rank,data
+
