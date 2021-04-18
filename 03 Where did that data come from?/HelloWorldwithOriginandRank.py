5b6db6cdc861fed59260937f669c3e0cdea71ff8
joshuah459 <joshuah459@gmail.com>
2021-04-18 17:10:16 -0500

    Update HelloWorldwithOriginandRank.py
    final tutorial version

--- 03 Where did that data come from?/HelloWorldwithOriginandRank.py
+++ 03 Where did that data come from?/HelloWorldwithOriginandRank.py
@@ -1,5 +1,10 @@
 from mpi4py import MPI
+
 comm = MPI.COMM_WORLD
 rank = comm.rank
+
 name=MPI.Get_processor_name()
-print(("Hello World from:",name,"whose rank is",rank))
+
+data = "Hello World"
+
+print data, " from:",name," whose rank is ",rank
